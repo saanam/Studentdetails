bf009303612c373474b4ce7b0bf5e68b6a794c2b
sanam <shrestha722@gmail.com>
2019-11-20 08:40:26 +0545

    Initial commit

---
+++ app/src/main/java/com/example/studentdetails/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.studentdetails;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Spinner spincountry;
+    private AutoCompleteTextView actv1;
+    private String[] batch = {"22A","22B","22C"};
+    private Button btnsave;
+    TextView tvoutputname, tvoutputgender, tvoutputcountry, tvoutputbatch;
+    RadioGroup rdgroup;
+    EditText etname;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spincountry = findViewById(R.id.spincountry);
+        actv1 = findViewById(R.id.actv1);
+        btnsave = findViewById(R.id.btnsave);
+        etname = findViewById(R.id.etname);
+        tvoutputname = findViewById(R.id.tvoutputname);
+        tvoutputgender = findViewById(R.id.tvoutputgender);
+        tvoutputcountry = findViewById(R.id.tvoutputcountry);
+        tvoutputbatch = findViewById(R.id.tvoutputbatch);
+        rdgroup = findViewById(R.id.rdgroup);
+
+        String countries[] = {"Nepal","China","India"};
+        ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, countries);
+        spincountry.setAdapter(adapter);
+
+        ArrayAdapter stringadapter = new ArrayAdapter<>(this, android.R.layout.select_dialog_item, batch);
+        actv1.setAdapter(stringadapter);
+        actv1.setThreshold(1);
+
+        btnsave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    tvoutputname.setText(String.valueOf(etname.getText()));
+                    RadioButton selectgender = findViewById(rdgroup.getCheckedRadioButtonId());
+                    tvoutputgender.setText(String.valueOf(selectgender.getText()));
+                    tvoutputcountry.setText(String.valueOf(spincountry.getSelectedItem()));
+                    tvoutputbatch.setText(String.valueOf(actv1.getText()));
+                }
+                catch (Exception e)
+                {
+                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+
+        });
+    }
+}
