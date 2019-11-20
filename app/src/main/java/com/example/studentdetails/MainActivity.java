9c7c2dcbc09e0f27bd3b526b68ead82bb2eb8a85
sanam <shrestha722@gmail.com>
2019-11-20 11:18:56 +0545

    final

--- app/src/main/java/com/example/studentdetails/MainActivity.java
+++ app/src/main/java/com/example/studentdetails/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.studentdetails;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -24,11 +26,13 @@ public class MainActivity extends AppCompatActivity {
     TextView tvoutputname, tvoutputgender, tvoutputcountry, tvoutputbatch;
     RadioGroup rdgroup;
     EditText etname;
+    AlertDialog.Builder builer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        builer = new AlertDialog.Builder(this);
 
         spincountry = findViewById(R.id.spincountry);
         actv1 = findViewById(R.id.actv1);
@@ -51,17 +55,33 @@ public class MainActivity extends AppCompatActivity {
         btnsave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try {
-                    tvoutputname.setText(String.valueOf(etname.getText()));
-                    RadioButton selectgender = findViewById(rdgroup.getCheckedRadioButtonId());
-                    tvoutputgender.setText(String.valueOf(selectgender.getText()));
-                    tvoutputcountry.setText(String.valueOf(spincountry.getSelectedItem()));
-                    tvoutputbatch.setText(String.valueOf(actv1.getText()));
-                }
-                catch (Exception e)
-                {
-                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
-                }
+                builer.setMessage("Do you want to save?").setCancelable(false)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                try {
+                                    tvoutputname.setText(String.valueOf(etname.getText()));
+                                    RadioButton selectgender = findViewById(rdgroup.getCheckedRadioButtonId());
+                                    tvoutputgender.setText(String.valueOf(selectgender.getText()));
+                                    tvoutputcountry.setText(String.valueOf(spincountry.getSelectedItem()));
+                                    tvoutputbatch.setText(String.valueOf(actv1.getText()));
+                                }
+                                catch (Exception e)
+                                {
+                                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+
+                AlertDialog alert = builer.create();
+                alert.setTitle("Save");
+                alert.show();
 
 
             }
